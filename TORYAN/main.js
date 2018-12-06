9e60b6a9f9eb6412cc53c5506b9eaf2563f6db1e
Aaron Shiffler <ajshiffler@gmail.com>
2018-12-05 19:05:03 -0700

    Some ideas of how entry, main, and test can all live and interest with eachother.

---
+++ TORYAN/main.js
@@ -0,0 +1,15 @@
+/*************************************************************************
+ * Module Description
+ *************************************************************************/
+module.exports = {
+    main (args) {
+        supportingFunction1 ();
+        supportingFunction2 ();
+        supportingFunction3 ();
+        return 0;
+    }
+};
+
+function supportingFunction1 () {}
+function supportingFunction2 () {}
+function supportingFunction3 () {}
